d08b31e8c37b71643a2f250c57b5ff2a4ca748b8
maatheuss0 <matheusaraujoms.machado@gmail.com>
2021-09-29 11:35:36 -0300

    pressa

--- SPMedical/Controllers/LoginController.cs
+++ SPMedical/Controllers/LoginController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SPMedical.Domains;
 using SPMedical.Interfaces;
 using SPMedical.Repositories;
+using SPMedical.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SPMedical.Controllers
@@ -19,5 +23,28 @@ namespace SPMedical.Controllers
             _usuarioRepository = new UsuarioRepository();
         }
         private IUsuarioRepository _usuarioRepository { get; set; }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+                if (usuarioBuscado == null)
+                {
+                    return BadRequest("Email ou senha incorretos");
+
+                    var minhasClaims = new[]
+                    {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email);
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.UsuarioId.ToString());
+                    new Claim(ClaimTypes.Role, usuarioBuscado.TipoUsuarioId.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
