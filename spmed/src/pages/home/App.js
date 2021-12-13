a7cb4dcf15449fb8b1cc08337d2535e231fb7d54
unknown <47926331848>
2021-12-13 10:47:04 -0300

    Css login iniciado

--- spmed/src/pages/home/App.js
+++ spmed/src/pages/home/App.js
@@ -10,14 +10,18 @@ function App() {
         <img src={hamburger} class = "hamburgerHeader"></img>
         <img src={logo} alt = "logo" class = "Logo"></img>
       </header>
-      <main>
+      <main class = "container">
         <section class="blocoLogin">
           <div>
             <span>FAÇA SEU LOGIN</span>
-            
             <input type = "email" placeholder = "Email"></input>
             <input type = "password" placeholder = "Senha"></input>
-            <button class="botaoLogin"></button>
+            <button class="botaoLogin">Login</button>
+          </div>
+          <div>
+            <a href="">Esqueceu a senha?</a>
+            <hr></hr>
+            <a href="">Não tem uma conta?</a>
           </div>
         </section>
       </main>
