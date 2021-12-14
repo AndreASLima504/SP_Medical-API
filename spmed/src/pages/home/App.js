1cba49e042a71fabbbc3c7e404558a0bdff8918e
unknown <47926331848>
2021-12-14 10:30:48 -0300

    Alterações no app css

--- spmed/src/pages/home/App.js
+++ spmed/src/pages/home/App.js
@@ -12,16 +12,18 @@ function App() {
       </header>
       <main class = "container">
         <section class="blocoLogin">
-          <div>
-            <span>FAÇA SEU LOGIN</span>
-            <input type = "email" placeholder = "Email"></input>
-            <input type = "password" placeholder = "Senha"></input>
-            <button class="botaoLogin">Login</button>
-          </div>
-          <div>
-            <a href="">Esqueceu a senha?</a>
-            <hr></hr>
-            <a href="">Não tem uma conta?</a>
+          <div class="blocoLoginContainer">
+            <div class="formularioLogin">
+              <span>FAÇA SEU LOGIN</span>
+            <input type = "email" placeholder = "Email" class="email"></input>
+              <input type = "password" placeholder = "Senha" class="senha"></input>
+              <button class="botaoLogin">Login</button>
+            </div>
+            <div>
+              <a href="">Esqueceu a senha?</a>
+              <hr></hr>
+              <a href="">Não tem uma conta?</a>
+            </div>
           </div>
         </section>
       </main>
